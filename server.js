b75d12b206d907d3f9c183b75995092094091e0b
skin93 <arro.ciesielski@gmail.com>
2020-01-16 13:55:17 +0100

    Test routes added

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
   res.send('API running');
 });
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
