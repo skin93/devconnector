2bc7e486307c28d27090d03fb3ea1125d3c4cc3b
skin93 <arro.ciesielski@gmail.com>
2020-01-16 16:45:18 +0100

    async, error, auth middlewares & JWT verify added

---
+++ middleware/auth.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+// protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // set token from Bearer token in header
+    token = req.headers.authorization.split(' ')[1];
+    // set token from cookie
+  }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  // make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Not authroize to access this route', 401));
+  }
+
+  try {
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.user.id);
+    next();
+  } catch (error) {
+    return next(new ErrorResponse('Not authroize to access this route', 401));
+  }
+});
+
+// grand access to specific roles
+// exports.authorize = (...roles) => {
+//   return (req, res, next) => {
+//     if (!roles.includes(req.user.role)) {
+//       return next(
+//         new ErrorResponse(
+//           `User role ${req.user.role} is not authorized to access this route`,
+//           403
+//         )
+//       );
+//     }
+//     next();
+//   };
+// };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -11,8 +13,13 @@ connectDB();
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-// Init middleware
-app.use(express.json({ extended: false }));
+// Body parser
+app.use(express.json());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('API running');
@@ -24,8 +31,17 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`.yellow.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
