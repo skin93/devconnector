a4e108588a31def21f91f2e1f1893060bf6943d4
skin93 <arro.ciesielski@gmail.com>
2020-01-20 17:04:29 +0100

    Adding comment added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from './types';
 
 // get posts
@@ -96,6 +98,47 @@ export const addPost = formData => async dispatch => {
     const res = await axios.post(`/api/posts`, formData, config);
     dispatch({ type: ADD_POST, payload: res.data });
     dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    if (err.response) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
+
+// add comment
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+    dispatch({ type: ADD_COMMENT, payload: res.data });
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    if (err.response) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
+
+// delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comments/${postId}/${commentId}`);
+    dispatch({ type: REMOVE_COMMENT, payload: commentId });
+    dispatch(setAlert('Comment Removed', 'success'));
   } catch (err) {
     if (err.response) {
       dispatch({
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import { withRouter } from 'react-router';
 import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false}></PostItem>
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
