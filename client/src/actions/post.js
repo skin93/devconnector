ab6c43f8179430e647aeec7eb31cdea74bf11215
skin93 <arro.ciesielski@gmail.com>
2020-01-20 15:58:41 +0100

    Deleting post added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 // get posts
 export const getPosts = () => async dispatch => {
@@ -37,6 +37,22 @@ export const removeLike = id => async dispatch => {
   try {
     const res = await axios.put(`/api/posts/unlike/${id}`);
     dispatch({ type: UPDATE_LIKES, payload: { id, likes: res.data } });
+  } catch (err) {
+    if (err.response) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
+
+// delete post
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({ type: DELETE_POST, payload: id });
+    dispatch(setAlert('Post Removed', 'success'));
   } catch (err) {
     if (err.response) {
       dispatch({
