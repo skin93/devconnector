a00c799f3133612c69b6b527ade9f298cac8633a
skin93 <arro.ciesielski@gmail.com>
2020-01-20 17:57:02 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -21,9 +22,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
+app.use(errorHandler);
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -31,7 +30,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-app.use(errorHandler);
+// serve static assets in production
+if (process.env.NODE_DEV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
